946d7375e74afd1f4b2bcdd8fd39bd7eb3d33ff3
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-11-20 09:20:14 +0700

    Connect the app to MongoDB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const Hapi = require('@hapi/hapi');
+const mongoDb = require('hapi-mongodb');
+const dotenv = require('dotenv');
 
 const init = async () => {
+  dotenv.config();
+
   const server = Hapi.server({
     port: process.env.PORT || 80,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -8,6 +12,18 @@ const init = async () => {
       cors: {
         origin: ['*'],
       },
+    },
+  });
+
+  // Configure MongoDb
+  await server.register({
+    plugin: mongoDb,
+    options: {
+      url: process.env.MONGODB_URI,
+      settings: {
+        useUnifiedTopology: true,
+      },
+      decorate: true,
     },
   });
 
