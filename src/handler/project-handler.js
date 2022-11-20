6ddee721dbcc0fe797b79adf5bb205fd01c768fa
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-11-20 17:24:48 +0700

    Add get projects feature

--- src/handler/project-handler.js
+++ src/handler/project-handler.js
@@ -1,5 +1,38 @@
 const { uploadIcon } = require('../util/cloudinary-util');
 
+const getProjects = async (request, h) => {
+  const { username } = request.auth.credentials;
+  let { page, size } = request.query;
+  let response = '';
+
+  try {
+    page = Number(page) || 1;
+    size = Number(size) || 10;
+
+    const projects = await request.mongo.db.collection('projects')
+      .find({ username })
+      .skip((page - 1) * size)
+      .limit(size)
+      .toArray();
+
+    response = h.response({
+      code: 200,
+      status: 'OK',
+      data: projects,
+    });
+  } catch (e) {
+    response = h.response({
+      code: 400,
+      status: 'Bad Request',
+      message: 'error',
+    });
+
+    response.code(400);
+  }
+
+  return response;
+};
+
 const uploadProject = async (request, h) => {
   const {
     title,
@@ -50,4 +83,4 @@ const uploadProject = async (request, h) => {
   return response;
 };
 
-module.exports = { uploadProject };
+module.exports = { getProjects, uploadProject };
