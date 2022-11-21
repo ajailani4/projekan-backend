b1cf3a2b044f481c978c9f56bdab7e20b59bd734
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-11-21 14:26:41 +0700

    Add update task feature

--- src/handler/task-handler.js
+++ src/handler/task-handler.js
@@ -48,4 +48,56 @@ const addTask = async (request, h) => {
   return response;
 };
 
-module.exports = { addTask };
+const updateTask = async (request, h) => {
+  const { id } = request.params;
+  const { ObjectID } = request.mongo;
+  const { title, status } = request.payload;
+  let response = '';
+
+  try {
+    // Check if task exists
+    const task = await request.mongo.db.collection('tasks').findOne({ _id: ObjectID(id) });
+
+    if (!task) {
+      response = h.response({
+        code: 404,
+        status: 'Not Found',
+        message: 'Task is not found',
+      });
+
+      response.code(404);
+
+      return response;
+    }
+
+    await request.mongo.db.collection('tasks')
+      .updateOne({ _id: ObjectID(id) }, {
+        $set: {
+          title,
+          status,
+        },
+      });
+
+    response = h.response({
+      code: 200,
+      status: 'OK',
+      message: 'Task has been updated',
+    });
+
+    response.code(200);
+
+    return response;
+  } catch (e) {
+    response = h.response({
+      code: 400,
+      status: 'Bad Request',
+      message: 'error',
+    });
+
+    response.code(400);
+  }
+
+  return response;
+};
+
+module.exports = { addTask, updateTask };
